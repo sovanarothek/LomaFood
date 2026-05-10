a13c5eb13805e718eb912e57b174b9a609546e2c
sovanarothek <eksovanaroth@gmail.com>
2026-05-10 23:36:36 +0700

    Clean project with proper gitignore

---
+++ services/rider-service/src/main/java/com/lomafood/riderservice/RiderServiceApplication.java
@@ -0,0 +1,15 @@
+package com.lomafood.riderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class RiderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RiderServiceApplication.class, args);
+    }
+}
